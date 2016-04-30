d9d0e9852c3a689609f5abacd94c58459cbaca78
domin1101 <dominik.winkelbauer@outlook.de>
2016-04-30 23:26:42 +0200

    [TASK] Adds again more evolution unit tests

---
+++ UnitTests/Learning/Evolution/PositiveMakerFitnessFunctionTest.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "Function/FermiFunction.hpp"
+#include <Mocks/MockmutationSelector.hpp>
+#include <Mocks/MockEvolutionObject.hpp>
+#include <Learning/Evolution/PositiveMakerFitnessFunction.hpp>
+
+class PositiveMakerFitnessFunctionTest : public testing::Test {
+public:
+	PositiveMakerFitnessFunction* positiveMakerFitnessFunction;
+
+	void SetUp() {
+		
+	}
+
+	virtual ~PositiveMakerFitnessFunctionTest()
+	{
+		delete positiveMakerFitnessFunction;
+	}
+};
+
+TEST_F(PositiveMakerFitnessFunctionTest, execute)
+{
+	positiveMakerFitnessFunction = new PositiveMakerFitnessFunction(10);
+
+	std::vector<std::pair<double, AbstractEvolutionObject*>> highscore;
+	highscore.push_back(std::make_pair(4, (AbstractEvolutionObject*)NULL));
+	highscore.push_back(std::make_pair(-3, (AbstractEvolutionObject*)NULL));
+	highscore.push_back(std::make_pair(-13, (AbstractEvolutionObject*)NULL));
+
+	positiveMakerFitnessFunction->execute(&highscore);
+	EXPECT_EQ(14, highscore[0].first);
+	EXPECT_EQ(7, highscore[1].first);
+	EXPECT_EQ(0, highscore[2].first);
+
+}
---
+++ UnitTests/Learning/Evolution/RandomCombiningStrategyTest.cpp
@@ -0,0 +1,72 @@
+#include "gtest/gtest.h"
+#include <Learning/Evolution/RandomCombiningStrategy.hpp>
+#include <Mocks/MockCoevolutionWorld.hpp>
+#include <Mocks/MockEvolutionObject.hpp>
+
+class RandomCombiningStrategyTest : public testing::Test {
+public:
+	RandomCombiningStrategy* randomCombiningStrategy;
+	MockCoevolutionWorld world;
+	MockEvolutionObject object1, object2, object3, object4, object5, object6;
+
+	void SetUp()
+	{
+		
+	}
+
+	virtual ~RandomCombiningStrategyTest()
+	{
+		delete randomCombiningStrategy;
+	}
+};
+
+TEST_F(RandomCombiningStrategyTest, executeEmptyWorld)
+{
+	randomCombiningStrategy = new RandomCombiningStrategy(3);
+
+	std::vector<AbstractEvolutionObject*> objects;
+	EXPECT_CALL(world, getEvolutionObjects()).WillRepeatedly(testing::Return(&objects));
+	
+	auto result = randomCombiningStrategy->execute(&world);
+
+	EXPECT_EQ(0, result.size());
+}
+
+TEST_F(RandomCombiningStrategyTest, executeSingleWorld)
+{
+	randomCombiningStrategy = new RandomCombiningStrategy(3);
+
+	std::vector<AbstractEvolutionObject*> objects({ &object1 , &object2, &object3 });
+	EXPECT_CALL(world, getEvolutionObjects()).WillRepeatedly(testing::Return(&objects));
+	EXPECT_CALL(world, compareObjects(testing::_, testing::_)).Times(9).WillRepeatedly(testing::Return(1));
+
+	auto result = randomCombiningStrategy->execute(&world);
+	
+	EXPECT_EQ(3, result.size());
+	EXPECT_EQ(3, result[&object1].size());
+	EXPECT_EQ(3, result[&object2].size());
+	EXPECT_EQ(3, result[&object3].size());
+}
+
+TEST_F(RandomCombiningStrategyTest, executeTwoWorlds)
+{
+	randomCombiningStrategy = new RandomCombiningStrategy(2);
+	
+	MockCoevolutionWorld parasiteWorld;
+	randomCombiningStrategy->setSecondWorld(&parasiteWorld);
+
+	std::vector<AbstractEvolutionObject*> objects({ &object1 , &object2, &object3 });
+	std::vector<AbstractEvolutionObject*> parasiteObjects({ &object4 , &object5, &object6 });
+
+	EXPECT_CALL(world, getEvolutionObjects()).WillRepeatedly(testing::Return(&objects));
+	EXPECT_CALL(parasiteWorld, getEvolutionObjects()).WillRepeatedly(testing::Return(&parasiteObjects));
+	EXPECT_CALL(world, compareObjects(testing::_, testing::_)).Times(6).WillRepeatedly(testing::Return(1));
+
+	auto result = randomCombiningStrategy->execute(&world);
+
+	EXPECT_EQ(5, result.size());
+	EXPECT_EQ(2, result[&object1].size());
+	EXPECT_EQ(2, result[&object2].size());
+	EXPECT_EQ(2, result[&object3].size());
+	EXPECT_EQ(6, result[&object4].size() + result[&object5].size() + result[&object6].size());
+}
